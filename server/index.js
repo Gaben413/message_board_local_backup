a145f7b2da2047014f3c94f70adf49512ba38af1
NowhereCat <gabrielbportela413@gmail.com>
2023-11-22 22:31:51 -0300

    Data collection update

--- server/index.js
+++ server/index.js
@@ -17,9 +17,16 @@ axios.get(url).then(res => {
                 console.log(`########## PAGE ${i+1} ##########`)
                 console.log(`Thread nº: ${threads[e]['no']}`)
                 console.log(`Thread Name: ${threads[e]['name']}`)
+                console.log(`Thread Creation Time UNIX: ${threads[e]['time']}`)
+                console.log(`Thread Creation Time: ${threads[e]['now']}`)
                 console.log(`Thread Sub: ${threads[e]['sub']}`)
                 console.log(`Thread Tag: ${threads[e]['tag']}`)
+                console.log(`Thread FileID: ${threads[e]['tim']}`)
+                console.log(`Thread File Link: https://i.4cdn.org/${board_name}/${threads[e]['tim'] + threads[e]['ext']}`)
+                console.log(`Thread Filename: ${threads[e]['filename'] + threads[e]['ext']}`)
+                console.log(`Thread Filesize: ${threads[e]['fsize']}`)
                 console.log(`Thread last_modified: ${threads[e]['last_modified']}`)
+                console.log(`Thread replies nº: ${threads[e]['replies']}`)
                 console.log(`Thread Link: https://boards.4channel.org/${board_name}/thread/${threads[e]['no']}`)
 
                 thread_id = threads[e]['no'];
@@ -34,8 +41,20 @@ axios.get(url).then(res => {
 
     axios.get(thread_url).then(res => {
         for (let i = 0; i < res.data['posts'].length; i++) {
-            console.log(res.data['posts'][i])
-            
+            //console.log(res.data['posts'][i])
+            console.log('@@@@@@@@@@@@@@@@@@@@')
+            console.log(`Post Link: https://boards.4channel.org/${board_name}/thread/${thread_id}#p${res.data['posts'][i]['no']}`)
+            console.log(`Post Number: ${res.data['posts'][i]['no']}`)
+            console.log(`Post Name: ${res.data['posts'][i]['name']}`)
+            console.log(`Post Date UNIX: ${res.data['posts'][i]['time']}`)
+            console.log(`Post Date: ${res.data['posts'][i]['now']}`)
+            console.log(`Post File Link: https://i.4cdn.org/${board_name}/${res.data['posts'][i]['tim'] + res.data['posts'][i]['ext']}`)
+            console.log(`Post File: ${res.data['posts'][i]['tim'] + res.data['posts'][i]['ext']}`)
+            console.log(`Post File Size: ${res.data['posts'][i]['fsize']}`)
+            console.log(`Post FileID: ${res.data['posts'][i]['tim']}`)
+            console.log(`Post Comment: ${res.data['posts'][i]['com']}`)
+
+            //if(i == 253) break;
         }
     }).catch(error => {
         console.log(error);
