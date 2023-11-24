2194abfcdad34f07e36b7b7ff35cc9b1f3124338
NowhereCat <gabrielbportela413@gmail.com>
2023-11-23 21:28:53 -0300

    Added database-management plan

---
+++ server/database-management.js
@@ -0,0 +1,28 @@
+//      CONNECTION
+
+/*
+    TABLES:
+        Thread Table
+        Post Table
+        Post Self Relation Table
+        Favourties
+*/
+
+//      SELECT METHODS
+
+//Check all Threads in DB. Returns Threads data.
+//Check for specific Thread in DB. Returns Thread data.
+//Check if Thread already exists. Returns true or false. (Maybe simply call last function and check if it does return something)
+
+//Check all Posts in selected Thread. Returns Posts data.
+//Compare last Post on thread. Returns true or false. 
+
+//      INSERT METHODS
+
+//Insert Thread data.
+//Insert Post data also insert reply relation into it's table, if there is one. Also check if Post number is already there, only post if it isn't.
+
+//      UPDATE METHODS
+
+//Update Thread table, change archived variable
+//Update favourites table
