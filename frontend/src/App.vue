a1509b65d3af542ce3ea3923574abfb382143338
Gaben <gabrielbportela413@gmail.com>
2024-01-23 14:25:27 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@
     -->
   </nav>
   <router-view/>
+  <button id="scrollbutton" v-on:click="topFunction">Top</button>
   <FooterComponent/>
 </template>
 
@@ -15,7 +16,27 @@
   export default{
     components: {
       FooterComponent,
-  },
+    },
+    mounted(){
+      let mybutton = document.getElementById("scrollbutton");
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function() {scrollFunction()};
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+    },
+    methods: {
+      // When the user clicks on the button, scroll to the top of the document
+      topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      } 
+    }
   }
 </script>
 
@@ -40,5 +61,24 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#scrollbutton {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  border: none;
+  outline: none;
+  background-color: green;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 10px;
+  font-size: 18px;
+}
+#scrollbutton:hover {
+  background-color: #555;
 }
 </style>
